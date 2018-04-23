81774c2c82f2ae59dfeb1c475b18db71077127a2
Lilia <1lilia.chicu@gmail.com>
2018-04-23 22:06:34 +0300

    111

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Circle
+{
+    int _Radius;
+    float _pi = 3.14F;
+public Circle(int Radius)
+    {
+        this._Radius = Radius;
+    }
+public float AreaCircle()
+    {
+        return this._pi * this._Radius * this._Radius;
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Circle C1 = new Circle(5);
+            float Area = C1.AreaCircle();
+            Console.WriteLine("Area is = {0}", Area);
+            Console.ReadLine();
+    
+        }
+    }
+}
