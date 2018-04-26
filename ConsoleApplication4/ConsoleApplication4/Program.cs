1799207201b6ac1bb3dbecc4a7db7973689ed0c2
Lilia <1lilia.chicu@gmail.com>
2018-04-26 21:53:40 +0300

    add new projetc test

---
+++ ConsoleApplication4/ConsoleApplication4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Student
+    {
+        private int _Id;
+        private string _Name;
+        public void SetId(int Id)
+        {
+            if (Id<=0)
+            {
+                throw new Exception("No negative values");
+            }
+            this._Id = Id;   
+        }
+        public int GetId()
+        {
+            return this._Id;      
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student C1 = new Student();
+            C1.SetId(-101);
+            Console.WriteLine(C1.GetId());
+        }
+
+       /* public static void Add(int i, int j)
+        {
+            Console.WriteLine(i + j);
+        }
+        public static void Add(double i, int j)
+        {
+            Console.WriteLine(i + j);
+        }*/
+    }
+}
