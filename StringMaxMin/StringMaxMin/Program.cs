ea0758ce68b1ec94c5be2c61754c9b5521a611f9
lchicu <lchicu@EN32193>
2018-09-28 10:05:30 +0300

    corect1

--- StringMaxMin/StringMaxMin/Program.cs
+++ StringMaxMin/StringMaxMin/Program.cs
@@ -4,11 +4,13 @@ namespace StringMaxMin
 {
     class Program
     {
-        public static void Main(string[] args)
-        {
         public string initialString = "Un nou rasarit, o noua zi";
         public char[] delimiters = new char[] { ' ', ',', '.', '!', '?', ':', ';' };
+        public static void Main(string[] args)
+        {
+        
         }
-
     }
+
+    
 }
