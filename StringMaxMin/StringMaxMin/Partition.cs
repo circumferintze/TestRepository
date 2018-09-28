149639e0995139f9386eb76e5e0388a5647de01f
lchicu <lchicu@EN32193>
2018-09-28 10:14:20 +0300

    new class g

---
+++ StringMaxMin/StringMaxMin/Partition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringMaxMin
+{
+    public class Partition
+    {
+        public string initialString = "Un nou rasarit, o noua zi";
+        public char[] delimiters = new char[] { ' ', ',', '.', '!', '?', ':', ';' };
+        public Partition(string input)
+        {
+            this.initialString = input;
+        }
+
+        public void PartitionOfString()
+        {
+            string[] partition = initialString.Split(delimiters);
+            foreach (var item in partition)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
