cff865edd104364117160a670fed7e071d644f6a
lchicu <lchicu@EN32193>
2018-10-01 09:50:01 +0300

    try another method to find max word

--- StringMaxMin/StringMaxMin/Partition.cs
+++ StringMaxMin/StringMaxMin/Partition.cs
@@ -17,16 +17,27 @@ namespace StringMaxMin
         public void PartitionOfString()
         {
             //bool verif;
-            List<string> partition = initialString.Split(delimiters).ToList();
-            var afterPartition = partition.Where(w => w.Length != 0).ToList();
-            var minWord = afterPartition.Where(w => w.Length == afterPartition.Min(x => x.Length)).ToList();
-            var maxWord = afterPartition.Where(w => w.Length == afterPartition.Max(y => y.Length)).ToList();
-            Console.WriteLine("Words with min legth : ");
+            List<string> partition = initialString.Split(delimiters, StringSplitOptions.RemoveEmptyEntries).ToList();
+            string word = "";
+            int n =0;
+            foreach (var item in partition)
+            {
+                if(item.Length > n)
+                {
+                    word = item;
+                    n = item.Length;
+                }
+            }
+            //var afterPartition = partition.Where(w => w.Length != 0).ToList();
+            /*var minWord = afterPartition.Where(w => w.Length == afterPartition.Min(x => x.Length)).ToList();
+            var maxWord = afterPartition.Where(w => w.Length == afterPartition.Max(y => y.Length)).ToList();*/
+
+            /*Console.WriteLine("Words with min legth : ");
             minWord.ForEach(a => Console.Write(a + " "));
             Console.WriteLine();
             Console.WriteLine("Words with max legth : ");
             maxWord.ForEach(a => Console.Write(a + " "));
-
+            */
 
             /*
             do
--- StringMaxMin/StringMaxMin/Program.cs
+++ StringMaxMin/StringMaxMin/Program.cs
@@ -8,7 +8,8 @@ namespace StringMaxMin
         public static void Main(string[] args)
         {
             Console.WriteLine("Insert your message");
-            string s = Console.ReadLine();
+            //string s = Console.ReadLine();
+            string s = "Love me tender love me true Elvis Presley";
             Partition partition = new Partition(s);
             partition.PartitionOfString();
             Console.ReadKey();
